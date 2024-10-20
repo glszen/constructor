6df69698f10ee233f78b3e64b3f54ae2cd29a733
Gülşen <gulsenk48@hotmail.com>
2024-10-20 17:15:39 +0300

    Add project files.

---
+++ Baby.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    public class Baby
+    {
+        public string name;
+        public string lastname;
+        public DateTime birthDate;
+
+
+        public Baby() //Default Constructor.
+        {
+            Console.WriteLine("Baby name is: Daisy " + "\nBaby lastname is: Pays " + "\nBaby birthdate is: " + "\nIngaaaaaaaaa " + DateTime.Now);
+        }
+
+        public Baby() //Alternative Constructor.
+        {
+            name = "Daisy";
+            lastname = "Pays";
+            birthDate = DateTime.Now;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//using constructor;
+//using System.Runtime.ExceptionServices;
+
+using constructor;
+
+Baby firstBaby = new Baby(); //Default Constructor.
+
+Baby secondBaby = new Baby(); //Alternative Constructor.
+Console.WriteLine("Baby name is: " + secondBaby.name + "\nBaby lastname is: " + secondBaby.lastname + "\nBaby birthdate is: " + "\nIngaaaaaaaaa " + secondBaby.birthDate);
